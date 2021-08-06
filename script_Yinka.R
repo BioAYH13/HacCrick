2a85a0fbbe0190c03e79bb8a78a79470bd366a91
BioAYH13 <ayinka13@gmail.com>
2021-08-06 09:36:48 +0100

    R script

---
+++ script_Yinka.R
@@ -0,0 +1,11 @@
+Name <- ("Atanda Yinka Habeeb")
+Email <- ("Ayinka13@gmail.com")
+Slack_username <- ("@Atanda_Yinka")
+Biostack <- ("Genomics")
+Twitter_username <- ("@Bio_AYH")
+Hamming_distance <- stringdist::afind(Slack_username,Twitter_username, window = NULL, value = F, method = "hamming", useBytes = FALSE)
+ayh <-  c(Name, Email, Slack_username, Biostack, Twitter_username, Hamming_distance$distance)
+knitr::combine_words(ayh, sep = ", ", and = "")
+
+
+
