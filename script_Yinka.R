f291a70abf86444e39b380d2cea76761220b70fa
BioAYH13 <79450940+BioAYH13@users.noreply.github.com>
2021-08-06 15:12:42 +0100

    Update script_Yinka.R

--- script_Yinka.R
+++ script_Yinka.R
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
-# This Rscript prints out my details
-my_details <-c("Atanda Yinka","ayinka13@gmail.com","@Atanda_Yinka","Genomics","@Bioayh13", "11")
+
+name <- "Atanda Yinka"
+email <- "Ayinka13@gmail.com"
+language <- "R"
+biostack <- "Genomics"
+slack <- "@Atanda_Yinka"
+
+my_details <- c(name, email, language, biostack, slack)
+  
 cat(paste(my_details, collapse = ','))
