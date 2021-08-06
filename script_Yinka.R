ef86d913151c96c476caf9078f6984677102f76f
BioAYH13 <79450940+BioAYH13@users.noreply.github.com>
2021-08-06 15:03:08 +0100

    Update script_Yinka.R

--- script_Yinka.R
+++ script_Yinka.R
@@ -1,4 +1,4 @@
-#!/usr/local/bin/Rscript
+#!/usr/bin/bash/Rscript
 # This Rscript prints out my details
 
 my_details <-c("Atanda Yinka","ayinka13@gmail.com","@Atanda_Yinka","Genomics","@Bioayh13", "11")
