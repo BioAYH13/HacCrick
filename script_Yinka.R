6d2a748c81dfb74708b073ec320840c3855c379b
BioAYH13 <79450940+BioAYH13@users.noreply.github.com>
2021-08-06 14:54:56 +0100

    Update script_Yinka.R

--- script_Yinka.R
+++ script_Yinka.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-Name <- "Atanda Yinka Habeeb"
-Email <- "Ayinka13@gmail.com"
-Slack_username <- "@Atanda_Yinka"
-Biostack <- "Genomics"
-Twitter_username <- "@Bio_AYH"
+Name <- ("Atanda Yinka Habeeb")
+Email <- ("Ayinka13@gmail.com")
+Slack_username <- ("@Atanda_Yinka")
+Biostack <- ("Genomics")
+Twitter_username <- ("@Bio_AYH")
 Hamming_distance <- stringdist::afind(Slack_username,Twitter_username, window = NULL, value = F, method = "hamming", useBytes = FALSE)
 ayh <-  c(Name, Email, Slack_username, Biostack, Twitter_username, Hamming_distance$distance)
 knitr::combine_words(ayh, sep = ", ", and = "")
